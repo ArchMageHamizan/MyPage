41051a17de902f7d3aa19564b93246ea4059f5c1
ArchMageHamizan <hamizanyus000@gmail.com>
2024-01-19 17:02:56 +0800

    change_1

--- resources/views/chronicle.blade.php
+++ resources/views/chronicle.blade.php
@@ -1,7 +1,28 @@
-<h1>My Story</h1>
-@include('project')
-
-<x-buss-card data="My Story"/>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>MY STORY</title>
+        <link rel="stylesheet" href="{{asset('css/chronicle/layout/layout.css')}}">
+    </head>
+    <body>
+        <div class="container">
+            <section class="one">
+                <h1>SPM</h1>
+            </section>
+        </div>
+        <section class="two">
+                <h1>Matrikulas</h1>
+            </section>
+            <section class="three">
+                <h1>Universiti</h1>
+            </section>
+            <section class="four">
+                <h1>Projek</h1>
+            </section>
+    </body>
+</html>
 
 
 
