a5a3be6cd8b1faa4a8a7b7937def3bd131e57287
ArchMageHamizan <hamizanyus000@gmail.com>
2024-02-07 16:35:59 +0800

    update_5

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -6,17 +6,30 @@ use Illuminate\Http\Request;
 use App\Models\Game; //use Game Model
 class GameController extends Controller
 {
+    
+    // Show data with pagination =============================================
     function displayList(){
         $data=Game::paginate(4);
         return view('game',['gameList'=>$data]);
     }
+    // Show data with pagination =============================================
+
+    //delete technique =======================================================
     function deleteData($id){
-        $data=Game::find($id);//find column named 'id'
+        $data=Game::find($id); //find column named 'id'
         $data->delete();
         return redirect('game');
     }
-    function updateData($id){
-         return Game::find($id);//find column named 'id'
-         
+    //=========================================================================
+
+    //update technique ========================================================
+    function showData($id){ //fetch data from table and display at form
+        return Game::find($id); //find column named 'id'
+
     }
+    function updateData($id){ //enable the fetched data being edit.
+        return Game::find($id); //find column named 'id'
+
+   }
+    //=========================================================================
 }
