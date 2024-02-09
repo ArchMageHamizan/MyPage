a379e354062bea63abd31651afadf1d69cd06c71
ArchMageHamizan <hamizanyus000@gmail.com>
2024-02-09 16:32:36 +0800

    update_8

--- app/Http/Controllers/AccessoriesController.php
+++ app/Http/Controllers/AccessoriesController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Facades\DB; //using DB class
 
 class AccessoriesController extends Controller
 {
     function dbNoModel(){
-        return "hello";
+        return DB::table('accessories');//fetch accessories table using table function from DB class;
     }
 }
